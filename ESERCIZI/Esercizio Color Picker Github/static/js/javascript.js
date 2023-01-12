a6a81bd82472d311db2c890d9111d44209d83dfb
MarCoPiteo <31marcopiteo31@gmail.com>
2023-01-12 14:58:33 +0100

    Work Done. Perfezionamenti e fix bug

--- ESERCIZI/Esercizio Color Picker Github/static/js/javascript.js
+++ ESERCIZI/Esercizio Color Picker Github/static/js/javascript.js
@@ -1,58 +1,16 @@
-let width = 600
-let height = 300
-let d = 80
+let link = document.querySelectorAll(".colorLink")
+console.log(link)
 
-let xA = 300
-let yA = 150
+for (let i=0; i<link.length; i++) {
+	let l = link[i]
 
-let direzione = 1
+	l.addEventListener("click", function(e) {
+		e.preventDefault()
 
-let origineX = 0
-let origineFinalCircle = width
-let hiddenIterationFlower= origineX - xA
+		let el = e.currentTarget
+		console.log(el.dataset.bgcolor)
 
-function setup () {
-	createCanvas(width, height)
-}
-
-function drawFlower(x, y, d) {
-	let r = d/2
-
-	stroke("rgb(0,0,0)")
-	strokeWeight(2)
-
-	fill("rgb(255,165,0)")
-	circle(x-r, y-r, d)
-	circle(x+r, y+r, d)
-	circle(x-r, y+r, d)
-	circle(x+r, y-r, d)
-	fill("rgb(255,51,0)")
-	circle(x, y, d)
-}
-
-function draw() {
-	
-	background(0,255,0)
-	hiddenIterationFlower = hiddenIterationFlower + direzione
-	xA = xA + direzione
-	origineX = origineX + direzione
-	origineFinalCircle = origineFinalCircle + direzione
-
-
-	
-	drawFlower(hiddenIterationFlower, yA, d) //FIORE NASCOSTO PER IL MOVIMENTO
-	drawFlower(xA, yA, d)					 //FIORE CENTRALE
-	drawFlower(origineX, yA, d)				 //FIORE DI SINISTRA
-	drawFlower(origineFinalCircle, yA, d) 	 //FIORE DI DESTRA
-
-	if (xA == 600) {
-		origineX = 0
-		xA = 300
-
-		hiddenIterationFlower = origineX - xA
-		origineFinalCircle = width
-		let iterazioneFlowerOrigine = hiddenIterationFlower - xA
-
-		drawFlower(iterazioneFlowerOrigine, yA, d)		//FIORE NASCOSTO CHE ESCE DA SINISTRA*/
-	}
+		let body = document.querySelector("body")
+		body.style.background = el.dataset.bgcolor
+	})
 }
