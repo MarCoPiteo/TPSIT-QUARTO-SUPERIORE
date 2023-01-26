d675ca09218e1d773272ff71a2d544739d1e73cf
MarCoPiteo <31marcopiteo31@gmail.com>
2023-01-26 16:09:21 +0100

    Aggiornamento

--- WEB DESIGN/WebPage Tris Game GitHub/static/js/javascript.js
+++ WEB DESIGN/WebPage Tris Game GitHub/static/js/javascript.js
@@ -1,31 +1,29 @@
 /*FINIRE FUNZIONE WIN AND DRAW. 
-FINIRE FUNZIONE CHE TOGLIE LE MOSSE AGLI AVVERSARI
 FARE DESIGN PAGINA HOME DI SCELTA PERSONAGGIO E BACKGROUND
-FINIRE ANCHE BACKGROUND TRIS TABLE
-FINIRE INDICATORE PERSONAGGIO CHE INIZIA
-FINIRE CAMBIO NUMERO VITTORIE AI PLAYER*/
+FINIRE ANCHE BACKGROUND TRIS TABLE (90%)---> DA CAPIRE SE VA TENUTO IL REATTORE DI BACKGROUND
+FINIRE INDICATORE PERSONAGGIO CHE INIZIA (80%) ---> AGGIUSTARE NUMERO DI VOLTE CHE SI RIPETE, E QUANDO L'AVVERSARIO 
+											  FA LA MOSSA, BLOCCARE L'ANIMAZIONE. DA VEDERE COL PROF
+*/
 
 
 let link = document.querySelectorAll(".pitchSquare")
 let restart = document.querySelector(".btnLink")
 
-//let player1Life = document.querySelectorAll 	DA FINIRE
+let currentPlayer = "null"
+currentPlayer = startPlayer()
 
-let currentPlayer
-let r = Math.floor(Math.random() *2)
+let playerLifeRemove
 
-if (r === 0) {		//PLAYER 1(IRONMAN) = O, PLAYER 2(THANOS) = X
-	currentPlayer = "O"
-} else {
-	currentPlayer = "X"
-}
-	
+let player1Wins = 0
+let player2Wins = 0
+
+
+
 for (let i = 0; i < link.length; i++) {
 	let l = link[i]
-
 	l.addEventListener("click", function (e) {
 		e.preventDefault()
-
+		
 		let win = false
 		let draw = false
 		let casella = e.currentTarget
@@ -35,10 +33,13 @@ for (let i = 0; i < link.length; i++) {
 				casella.classList.add("player1Play")
 
 				casella.dataset.stato = currentPlayer
+				removeMove(currentPlayer)
 
 				win = checkWinner(currentPlayer)
 				if (win === true) {
 					//win() 		SVILUPPARE FUNZIONE PER DARE LA VITTORIA
+
+					winUpdate()
 					restartCleaning()
 				} else {
 					draw = fullCellsCheck()
@@ -46,20 +47,22 @@ for (let i = 0; i < link.length; i++) {
 					if (draw === true) {
 						//draw		SVILUPPARE FUNZIONE PER DARE IL PAREGGIO
 						restartCleaning()
-
 					} else {
 						currentPlayer = "X"
-					}
 
+					}
 				}
 			} else {
 				casella.classList.add("player2Play")
 
 				casella.dataset.stato = currentPlayer
+				removeMove(currentPlayer)
 
 				win = checkWinner(currentPlayer)
 				if (win === true) {
 					//win() 		SVILUPPARE FUNZIONE PER DARE LA VITTORIA
+
+					winUpdate()
 					restartCleaning()
 				} else {
 					draw = fullCellsCheck()
@@ -67,24 +70,41 @@ for (let i = 0; i < link.length; i++) {
 					if (draw === true)  {
 						//draw		SVILUPPARE FUNZIONE PER DARE IL PAREGGIO
 						restartCleaning()
-
 					} else {
 						currentPlayer = "O"
 					}
-
 				}
-			}	
+			} 
+			animationPlayer(currentPlayer)
 		}
 	})
+}
+restart.addEventListener("click", function(e) {			//CHIEDERE IL PERCHE, SE METTO L'EVENTLISTENER DEL RESTART, NEL FOR PRINCIPALE, VIENE MANDATO IN ESECUZIONE PIU VOLTE, SFASANDO L'ANIMAZIONE. FUORI DA QUEL FOR, INVECE NO.
+	e.preventDefault()
 
-	restart.addEventListener("click", function(e) {
-		e.preventDefault()
+	restartCleaning()
+})
 
-		restartCleaning()
-	})
+
+function winUpdate() {
+	let winOutput
+
+	if (currentPlayer === "O") {
+		player1Wins += 1
+
+		winOutput = document.querySelector(".player1Win")
+		winOutput.innerHTML = player1Wins
+	} else if (currentPlayer === "X") {
+		player2Wins += 1
+
+		winOutput = document.querySelector(".player2Win")
+		winOutput.innerHTML = player2Wins
+	}
 }
 
 
+
+//FUNZIONI
 
 function restartCleaning() {
 	for (let i = 0; i < link.length; i++) {
@@ -95,6 +115,111 @@ function restartCleaning() {
 		}
 		
 		link[i].dataset.stato = "null"
+	}
+
+	playerLifeRemove = document.querySelectorAll(".functionRemoveMove")
+
+	for (let i = 0; i < playerLifeRemove.length; i++) {
+		playerLifeRemove[i].classList.remove("removeMove")
+
+		playerLifeRemove[i].dataset.move = "0"
+	}	
+
+	currentPlayer = "null"
+	startPlayer()
+}
+
+
+function startPlayer() {
+	let r = Math.floor(Math.random() *2)
+
+	if (r === 0) {		//PLAYER 1(IRONMAN) = O, PLAYER 2(THANOS) = X
+		currentPlayer = "O"
+	} else {
+		currentPlayer = "X"		
+	}
+	animationPlayer(currentPlayer)
+
+	return currentPlayer
+}	
+
+
+function animationPlayer(currentPlayer) {
+const animateCSS = (element = '.playerImages', animation = 'pulse', prefix = 'animate__', 
+	duration = 'slow', repeating = 'repeat-1')
+
+  new Promise((resolve, reject) => {
+	let i = 0
+    const bounce = `${prefix}${animation}`;
+    const slower = `${prefix}${duration}`
+    const repeat = `${prefix}${repeating}`
+
+    const currentPlayerAnimation = document.querySelectorAll(element);
+
+    if (currentPlayer === "O") {
+    	i = 0
+    } else if (currentPlayer === "X") {
+    	i = 1
+    }
+
+	currentPlayerAnimation[i].classList.add(`${prefix}animated`, bounce, slower, repeat);
+
+    function handleAnimationEnd(event) {
+		event.stopPropagation();
+		currentPlayerAnimation[i].classList.remove(`${prefix}animated`, bounce, slower, repeat);
+		resolve('Animation ended');
+    }
+
+    currentPlayerAnimation[i].addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
+
+
+
+
+
+
+
+	/*if (currentPlayer === "O") {
+		currentPlayer1.classList.add("currentPlayer")
+
+		currentPlayer2.classList.remove("currentPlayer")
+	} else if (currentPlayer === "X") {
+		currentPlayer2.classList.add("currentPlayer")
+
+		currentPlayer1.classList.remove("currentPlayer")
+	}*/
+}
+
+
+function removeMove(currentPlayer) {
+	if (currentPlayer === "O") {
+		playerLifeRemove = document.querySelectorAll(".player1LifeImage")
+	} else if (currentPlayer === "X") {
+		playerLifeRemove = document.querySelectorAll(".player2LifeImage")
+	}
+
+	for (let i = 0; i < playerLifeRemove.length; i++) {
+		if (playerLifeRemove[i].dataset.move === "0") {
+			playerLifeRemove[i].classList.add("removeMove")
+
+			playerLifeRemove[i].dataset.move = "1"
+			return
+		}
+	}	
+}
+
+
+function fullCellsCheck() {
+	let fullCells = 0
+
+	for (let i = 0; i < link.length; i++) {
+		if (link[i].dataset.stato != "null") {
+			fullCells += 1
+		}
+
+		if (fullCells >= 8) {
+			return true
+		}
 	}
 }
 
@@ -135,19 +260,4 @@ function checkWinner(currentPlayer) {
 	} else {
 		return false
 	} 
-}
-
-
-function fullCellsCheck() {
-	let fullCells = 0
-
-	for (let i = 0; i < link.length; i++) {
-		if (link[i].dataset.stato != "null") {
-			fullCells += 1
-		}
-
-		if (fullCells >= 8) {
-			return true
-		}
-	}
 }
